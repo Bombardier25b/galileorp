83c35860136bb60aee604f4ec276d02b8308f9da
Bombardier25b <102481793+Bombardier25b@users.noreply.github.com>
2022-03-27 17:22:14 -0400

    v1.1

---
+++ [esx_addons]/esx_jobs/fxmanifest.lua
@@ -0,0 +1,25 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'ESX Job Listing'
+
+version '1.6.5'
+
+shared_scripts {
+	'@es_extended/imports.lua',
+	'@es_extended/locale.lua',
+	'config.lua',
+	'jobs/*.lua',
+	'locales/*.lua',
+}
+server_scripts {
+	'@oxmysql/lib/MySQL.lua',
+	'server/main.lua'
+}
+
+client_scripts {
+	'client/main.lua'
+}
+
+dependency 'es_extended'
